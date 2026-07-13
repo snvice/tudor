5c3be32592f0bb7b29b8d393e5794ce3aecf700b
snvice <131165228+snvice@users.noreply.github.com>
2026-07-13 23:52:47 +0300

    improve the hamburger menu

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 document.addEventListener('DOMContentLoaded', function () {
     const toggleButton = document.querySelector('.navbar .mobile-menu-toggle');
     const mobileMenu = document.querySelector('.navbar .mobile-menu-items');
+    const backdrop = document.querySelector('.navbar .backdrop');
 
     toggleButton.addEventListener('click', function () {
-        mobileMenu.classList.toggle('active')
+        mobileMenu.classList.toggle('active');
+        backdrop.classList.toggle('active');
+    })
+
+    backdrop.addEventListener('click', function () {
+        mobileMenu.classList.toggle('active');
+        backdrop.classList.toggle('active');
     })
 });

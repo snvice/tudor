c245a9cdf6a1b9d600584bf6c7229134c3f83c04
snvice <131165228+snvice@users.noreply.github.com>
2026-07-15 00:00:01 +0300

    add navbar background on scroll

--- script.js
+++ script.js
@@ -28,3 +28,13 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+// change navbar background on scroll
+window.addEventListener('scroll', function () {
+  const navbar = document.querySelector('.navbar');
+  if(window.scrollY > 0) {
+    navbar.classList.add('navbar-scroll');
+  } else {
+    navbar.classList.remove('navbar-scroll');
+  }
+})
